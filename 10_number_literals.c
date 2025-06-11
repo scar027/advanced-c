701d6e428fe1ef2972c2c10e4e0047905073e3d7
scar027 <shantanupai@hotmail.com>
2025-06-11 11:58:17 +0530

    add: number literals
    Signed-off-by: scar027 <shantanupai@hotmail.com>

---
+++ 10_number_literals.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <stdint.h>
+
+int main(int argc, char **argv) {
+        int64_t a = 9223372036854775807;
+        int b = 0xFFAA01BB;
+        int c = 0755;
+        int d = 0b01010101;
+
+        printf("Signed 64 bit integer: %ld\n", a);
+        printf("Hexadecimal: %x\n", b);
+        printf("Octal: %o\n", c);
+        printf("Binary: %b\n", d);
+        return 0;
+}
