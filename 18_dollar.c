0b652f8c9e96df4f403901f21044deb00648a59a
scar027 <shantanupai@hotmail.com>
2025-06-16 12:43:51 +0530

    feat: add dollar
    Signed-off-by: scar027 <shantanupai@hotmail.com>

---
+++ 18_dollar.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+        int legendary$discovery$ = 5;
+        printf("Legendary discovery: %d\n", legendary$discovery$);
+        return 0;
+}
