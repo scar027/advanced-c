39deee27618df8ead4d6756bc8506bdd9a882031
scar027 <shantanupai@hotmail.com>
2025-06-16 18:50:42 +0530

    feat: add read a file line by line with getline
    Signed-off-by: scar027 <shantanupai@hotmail.com>

---
+++ 21_read_a_file_line_with_getline.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <malloc.h>
+
+int main(int argc, char **argv)
+{
+        FILE *f;
+        char *line;
+        size_t line_buffer_len;
+        ssize_t read_size;
+        
+        f = fopen("dummy_03.txt", "r");
+        assert(f != NULL);
+
+        line = NULL;
+        line_buffer_len = 0;
+        read_size = 0;
+        while ((read_size = getline(&line, &line_buffer_len, f)) != -1) {
+                line[read_size - 1] = '\0';
+                printf("Line = %s\n", line);
+        }
+        assert(feof(f));
+
+        free(line);
+        fclose(f);
+        return 0;
+}
