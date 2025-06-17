3163989e4d1ca34a8158a218f8b19932d06d2f19
scar027 <shantanupai@hotmail.com>
2025-06-17 23:04:30 +0530

    feat: add extern "C"
    Signed-off-by: scar027 <shantanupai@hotmail.com>

---
+++ 26_extern_c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "26_lib.h"
+
+int main(int argc, char **argv)
+{
+        int res;
+
+        res = multiply(3, 4);
+        printf("Result = %d\n", res);
+        return 0;
+}
---
+++ 26_lib.h
@@ -0,0 +1,9 @@
+#ifndef LIB_H
+#define LIB_H
+
+#ifdef __cplusplus
+extern "C"
+#endif
+int multiply(int, int);
+
+#endif
