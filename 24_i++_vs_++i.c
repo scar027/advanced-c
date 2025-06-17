2d6622c83357e94bcd702a9e44aa15990c7364a8
scar027 <shantanupai@hotmail.com>
2025-06-17 13:14:57 +0530

    feat: add i++ vs ++i
    Signed-off-by: scar027 <shantanupai@hotmail.com>

---
+++ 24_i++_vs_++i.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+        int a[5] = {1, 2, 3, 4, -1};
+        int i;
+
+        printf("Post-increment: \n");
+        i = 0;
+        while (a[i] != -1)
+                printf("%d ", a[i++]);
+        printf("\n");
+        
+        printf("Pre-increment: \n");
+        i = 0;
+        while (a[i] != -1)
+                printf("%d ", a[++i]);
+        printf("\n");
+        /* Note: i += 1 is equivalent to ++i */
+        return 0;
+}
