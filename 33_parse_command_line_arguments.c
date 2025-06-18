692d333f6ac1525e5ad68559f9df28006cd47d04
scar027 <shantanupai@hotmail.com>
2025-06-18 13:44:06 +0530

    feat: add parse command line arguments
    Signed-off-by: scar027 <shantanupai@hotmail.com>

---
+++ 33_parse_command_line_arguments.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+        int option;
+        int i;
+        
+        /* try passing flags -a foo -b bar baz qux quux -c to test */
+        while ((option = getopt(argc, argv, ":a:b")) != -1) {
+                switch (option) {
+                case 'a':
+                        printf("Flag a received: %s\n", optarg);
+                        break;
+                case 'b':
+                        printf("Flag b received\n");
+                        break;
+                case '?':
+                        printf("Error\n");
+                        break;
+                case ':':
+                        printf("Expected arguments\n");
+                        break;
+                }
+        }
+        for (i = optind; i < argc; i++)
+                printf("%d: %s\n", i, argv[i]);
+        return 0;
+}
